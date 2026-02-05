bb13dd7a378da48bfa1f64695d61f5a439f6ee62
Ankit Puri <your-email@example.com>
2026-02-05 23:06:02 +0545

    Alright this will be it for today

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
    <>
-   {/*The css color definetly needs some changes so that is also main priority */}
+   {/*The css color definetly needs some changes so that is also main priority. We will continue when we get break from professional classes*/}
    <Hero/>
    <section  id="FieldForComplaint">
     <FieldForComplaint/>
